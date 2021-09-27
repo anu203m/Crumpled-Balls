1a014b5d5725efce6ba261282148f428a997776c
anu203m <78315583+anu203m@users.noreply.github.com>
2021-09-27 19:07:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,13 +4,9 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
-var dustbinObj,groundObject, paper;
+var dustbinObj, paperObject,groundObject	
 var world;
 
-
-function preload(){
-
-}
 
 function setup() {
 	createCanvas(1600, 700);
@@ -20,11 +16,24 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 	
+	paperObject=new paper(200,450,70);
 	groundObject=new ground(width/2,670,width,20);
 	dustbinObj=new dustbin(1200,650);
-	paper = new Paper(1100,550,10);
+	//Create a Ground
+	
+
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1600,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
 
 	Engine.run(engine);
+	Render.run(render);
   
 }
 
@@ -32,15 +41,17 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(230);
-
-
-
-
-
-
+ 
+  
+  paperObject.display();
   groundObject.display();
   dustbinObj.display();
-  paper.display();
 
+  
+  
+ 
+  
+  
+ 
 }
 
